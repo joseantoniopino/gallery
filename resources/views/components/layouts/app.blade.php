215d15c1fc6b69bf8ddc63d5449760b66980561d
JoseA <josanpino@gmail.com>
2023-09-13 23:18:45 +0200

    build(assets): Install TALL

---
+++ resources/views/components/layouts/app.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+        <title>{{ $title ?? 'Gallery' }}</title>
+
+        <link rel="preconnect" href="https://fonts.bunny.net">
+        <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
+
+        @vite(['resources/css/app.css', 'resources/js/app.js',])
+    </head>
+    @livewireStyles
+    <body>
+        <main>
+            {{ $slot }}
+        </main>
+        @livewireScripts
+    </body>
+</html>
